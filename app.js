b9707175013925de9c5f93f2e051a3c1c4cc1be5
rootlinux777 <gsvmch777@gmail.com>
2025-04-29 00:10:12 +0600

    app.js files are some instruction like, learn Git and Github

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-
+/**
+ * 
+ * => Working Directory => Stage => Local Repository =>>> Remote Repository.
+ * 
+ * 
+ * => download git application from online throw Internet browser.
+ * => install git apllication in your own computer or laptop.
+ * 
+ * => github account create using own rules.
+ * => create git repository (Remote/Cloud).
+ * 
+ * 
+ * => git clone http://www.exampleUrl.com
+ * => git init (optional)
+ * 
+ * 
+ * 
+ * => git add fileName
+ * => git add . (current directory)
+ * => git add * (current directory without deleted files)
+ * 
+ * => git add -A (all files and folder)
+ * => git add --all (all files and folder)
+ * 
+ * => git add *.js (all .js extension files)
+ * 
+ * 
+ * 
+ * 
+ * => git reset 
+ * 
+ * => git commit -m "something message here"
+ * => git reset HEAD~
+ * 
+ * => git reset --hard
+ * => git revert (using forward and safety for team users)
+ * 
+ * 
+ * => git branch (Check branch name list)
+ * => git branch branchName (Create branch)
+ * => git checkout branchName (Switch branch)
+ * => git checkout -b development (Create and switch development branch)
+ * 
+ * => git merge main -m "Merging on development with main branch"
+ * 
+ * 
+ * => git push origin development (upload Remote repository)
+ * => git push origin main (upload development branch to Remote repository)
+ * 
+ * => git fetch (download changes)
+ * => git merge (fetch active in local repository)
+ * 
+ * => git pull (download and active directory changes)
+ * 
+ */
